dab77b01fe22918d64b3ef0e57ec48bb8c240ce7
Anivrit Subramaniam <anivrit@ucla.edu>
2022-04-16 01:47:25 -0700

    decision algo

---
+++ decision.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+
+def odds_to_probability(odds):
+    if odds < 0:
+        odds_adjusted = odds*(-1)
+        p_adjusted = odds_adjusted/(odds_adjusted+100)
+    else:
+        p_adjusted = 100/(odds_adjusted+100)
+    return p_adjusted
+
+def compute_odds(odd_hit,odd_miss):
+    adjusted_hit = odds_to_probability(odd_hit)
+    adjusted_miss = odds_to_probability(odd_miss)
+    house_edge = (adjusted_hit+adjusted_miss - 1) / 2
+    if house_edge < 0:
+        return adjusted_hit
+    else:
+        return (adjusted_hit-house_edge)
+
+def is_profitable(odds_list):
+    num_bets = len(odds_list)
+    multiplier = 1
+    if num_bets == 2:
+        multiplier = 3
+    elif num_bets == 3:
+        multiplier = 5
+    elif num_bets == 4:
+        multiplier = 10
+    adjusted_odds = []
+    for i in odds_list:
+        odds_hit_a = i[0]
+        odds_miss_a = i[1]
+        adjusted_a = compute_odds(odds_hit_a,odds_miss_a)
+        adjusted_odds.append(adjusted_a)
+    product = 1
+    for i in adjusted_odds:
+        product *= i
+    ev = multiplier*product-1
+    if ev>0:
+        return True,ev
+    else:
+        return False,ev
+        
